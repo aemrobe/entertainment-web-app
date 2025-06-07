04d4003b9cbdf0433167535a5c568a23ca7ae66b
aemrobe <aemrobekalu083@gmail.com>
2025-06-07 12:38:06 +0300

    fixing the base path

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import eslint from "vite-plugin-eslint";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react(), eslint()],
-  base: "/entertainment-web-app",
+  base: "/",
 });
